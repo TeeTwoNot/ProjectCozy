64d41f0640bf859ea29b18151dd88a7be918a336
TeeTwoNot <timeless.off.tv@gmail.com>
2023-11-02 14:50:45 +0200

    Add invite route

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Quart App file
 """
 
 import sys
-from quart import Quart, render_template
+from quart import Quart, render_template, redirect
 
 #DON'T GENERATE __PYCACHE__
 sys.dont_write_bytecode = True
@@ -16,3 +16,7 @@ app = Quart(__name__)
 @app.route("/")
 async def pageHome():
     return await render_template('index.html')
+
+@app.route("/invite")
+async def pageHome():
+    return await redirect('https://discord.com/api/oauth2/authorize?client_id=1168950370580578385&permissions=0&scope=applications.commands%20bot')
