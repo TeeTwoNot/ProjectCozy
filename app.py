8a31b5dd5cd3e78cf697c5b0c3613f1a219f90cd
TeeTwoNot <timeless.off.tv@gmail.com>
2023-11-02 10:47:20 +0200

    Add base quart renderer

--- app.py
+++ app.py
@@ -3,3 +3,16 @@ app.py
 -------
 Quart App file
 """
+
+import sys
+from quart import Quart, render_template
+
+#DON'T GENERATE __PYCACHE__
+sys.dont_write_bytecode = True
+
+#FLASK APP
+app = Quart(__name__)
+
+@app.route("/")
+async def pageHome():
+    return await render_template('index.html')
