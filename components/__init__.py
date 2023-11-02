c78f2c1b9d9604ac737f2f590e85fb849f17b049
TeeTwoNot <timeless.off.tv@gmail.com>
2023-11-02 20:59:06 +0200

    Quote list & folder with __init__

---
+++ components/__init__.py
@@ -0,0 +1,5 @@
+"""
+Components
+---
+Files that usually contain lists separated from the main files, for less clutter.
+"""
---
+++ components/lists.py
@@ -0,0 +1,11 @@
+"""
+lists.py
+---
+File containing lists.
+"""
+
+coffeequotes = [
+    "Coffee!",
+    "Who doesn't like coffee?",
+    "It's coffee time!"
+]
